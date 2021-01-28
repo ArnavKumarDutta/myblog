4f21e57665ceb7174008b6b427012c9650e2784b
ArnavKumarDutta <78157984+ArnavKumarDutta@users.noreply.github.com>
2021-01-28 19:58:22 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,26 @@
+console.log("Hi Im in console screen");
+var Last_Name = "Arnav";
+var First_Name = "Dutta";
+console.log(Last_Name);
+console.log(First_Name);
+var Full_Name = First_Name.concat(Last_Name);
+console.log(Full_Name);
+var Length_of_full_name = Full_Name.length;
+console.log(Length_of_full_name);
+var name_1 = "I am Jack Dutta";
+var replace_name = name_1.replace("Dutta","Sharma");
+console.log(replace_name);
+var C_Name = "CHRIS";
+var L_Name = C_Name.toLowerCase();
+console.log(L_Name);
+var Z_name = "sportz";
+var X_name = Z_name.toUpperCase();
+console.log(X_name)
+var square_root = Math.sqrt(16);
+console.log(square_root);
+var Maximum_Number = Math.max(15,10);
+console.log(Maximum_Number);
+var Minimum_Number = Math.min(15,10);
+console.log(Minimum_Number);
+var power_eg = Math.pow(4,2)
+console.log(power_eg);
